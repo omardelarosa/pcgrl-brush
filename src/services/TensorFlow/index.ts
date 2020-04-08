58067733044de0109413ebd708eb93b8461c1505
Hang Dong <hang@Hangs-MBP.fios-router.home>
2020-04-08 14:11:49 -0400

    load models

--- src/services/TensorFlow/index.ts
+++ src/services/TensorFlow/index.ts
@@ -1,8 +1,21 @@
 // Loading tensorflow ideally only at the app level
 import * as tf from "@tensorflow/tfjs";
-
 // Testing basic functionality of tensorflow using code from:
 // https://www.tensorflow.org/js/guide/tensors_operations
+
+let model = null;
+const MODEL_HTTP_URL = '/models-tfjs/sokoban/narrow/model_1/model.json';
+async function fetchModel() {
+    try {
+        console.log('url: ', window.location.href.split('src')[0] + MODEL_HTTP_URL)
+        let model = await tf.loadGraphModel(window.location.href.split('src')[0] + MODEL_HTTP_URL);
+        console.log(model);
+        return model;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 export class TensorFlowService {
     public static tensorFlowHelloWorld() {
         // Initiailize a demo tensor
@@ -12,6 +25,12 @@ export class TensorFlowService {
         ]);
         console.log("a shape:", a.shape);
         a.print();
+        // Test model
+        fetchModel().then(async modelResp => {
+            model = modelResp;
+            //predictAndDraw();
+            //game = setInterval(predictAndDraw, 50);
+        })
     }
 
     public static createMatrix(
