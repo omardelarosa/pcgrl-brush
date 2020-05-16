1b5efc3b46150068346d9a27e21f5c1b1d87b6f5
Omar Delarosa <thedelarosa@gmail.com>
2020-05-16 13:10:42 -0400

    adding generated map script

---
+++ tests/generate_assorted_maps.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Test map example for m00.json
+
+iterations=20 # number of iterations per map.
+
+for s in {1..5}
+do
+    for r in {1..3}
+    do
+        for i in {00..99}
+        do
+            m=$(printf "%02d" $i)
+            map_name="s${s}_r${r}_i${iterations}_m${m}"
+            echo "generating: $map_name"
+            ./node_modules/.bin/ts-node \
+                --project tsconfig.cli.json \
+                src/cli/index.ts \
+                --genMap \
+                --radius $r \
+                --steps $s \
+                --iterations $iterations \
+                --outputPath data/generated_maps/ \
+                --name $map_name
+        done
+    done
+done
+
