e1c4d5a5047cbf1cf5622f3973a9a9866c22c956
Omar Delarosa <thedelarosa@gmail.com>
2020-05-16 22:28:58 -0400

    name change reflected in the logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./styles.css";
 
 export function Logo() {
-    return <div className="logo rounded-container pixel-text">PCGRL Brush</div>;
+    return <div className="logo rounded-container pixel-text">RL Brush</div>;
 }
